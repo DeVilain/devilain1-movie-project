5ca631c78e99196c9225ece7ab1c07eedc4a466d
dat295 <jvdat15@gmail.com>
2021-02-17 10:43:36 +0700

    Updates

---
+++ precache-manifest.ef4aaba8f48016632ca2228f62937fa6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "078701219fb5b466f30f05792417d909",
+    "url": "/devilain1-movie-project/index.html"
+  },
+  {
+    "revision": "86b071f1a3ae681efa11",
+    "url": "/devilain1-movie-project/static/css/main.4557d174.chunk.css"
+  },
+  {
+    "revision": "ba5048cdb0eeb24534e4",
+    "url": "/devilain1-movie-project/static/js/2.2fbd713a.chunk.js"
+  },
+  {
+    "revision": "bbee919d8888a17953b5bb98359759ba",
+    "url": "/devilain1-movie-project/static/js/2.2fbd713a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86b071f1a3ae681efa11",
+    "url": "/devilain1-movie-project/static/js/main.607af195.chunk.js"
+  },
+  {
+    "revision": "ae5a39cc2d07564737ca",
+    "url": "/devilain1-movie-project/static/js/runtime-main.2bd10331.js"
+  },
+  {
+    "revision": "794fec8686e3ad9f4418536a87f924ea",
+    "url": "/devilain1-movie-project/static/media/svg-1.794fec86.svg"
+  },
+  {
+    "revision": "f48e58677849ac48f15bb6e9c3d7fbbd",
+    "url": "/devilain1-movie-project/static/media/svg-2.f48e5867.svg"
+  },
+  {
+    "revision": "3895a74e91c1d9c8bdfcb93c02f33880",
+    "url": "/devilain1-movie-project/static/media/svg-3.3895a74e.svg"
+  },
+  {
+    "revision": "d1e95708fcd79ba72fb6110a7e80653d",
+    "url": "/devilain1-movie-project/static/media/video2.d1e95708.mp4"
+  }
+]);
